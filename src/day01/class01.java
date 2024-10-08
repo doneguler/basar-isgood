ea7de44374b2ac4c4c8868afb6cddce3ee87671a
Döne Güler <doneguler173@gmail.com>
2024-10-08 22:22:52 +0300

    deneme

--- src/day01/class01.java
+++ src/day01/class01.java
@@ -4,6 +4,7 @@ public class class01 {
     public static void main(String[] args) {
         System.out.println("never give up");
         System.out.println("done işe başladı");
+        System.out.println("biz bu işi çözdük");
 
     }
 }
